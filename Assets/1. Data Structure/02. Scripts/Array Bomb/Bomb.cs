15b79cc0e3b5e632a3421a400e530103e0aad42d
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-07-08 15:59:15 +0900

    250708 ArrayBomb

---
+++ Assets/1. Data Structure/02. Scripts/Array Bomb/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private Rigidbody bombRb;
+    public float bombTime = 4f;
+    public float bombRange = 10f;
+    public float bombPower = 1000f;
+    public LayerMask layerMask;
+
+    private void Awake()
+    {
+        bombRb = GetComponent<Rigidbody>();
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(bombTime);
+
+        BombForce();
+    }
+
+    private void BombForce()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, bombRange, layerMask);
+
+        foreach (var collider in colliders)
+        {
+            Rigidbody rb = collider.GetComponent<Rigidbody>();
+
+            // AddExplosionForce( 폭발 파워, 폴발 위치, 폭발 범위, 폭발 높이)
+            rb.AddExplosionForce(bombPower, transform.position, bombRange, 1f);
+        }
+
+        Destroy(gameObject);
+    }
+}
