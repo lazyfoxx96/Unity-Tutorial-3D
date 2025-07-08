5fb07e6c7d47cac91432a2809d2a7cb74fe865d5
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-07-08 14:46:14 +0900

    250708 study dictionary

--- Assets/1. Data Structure/02. Scripts/StudyDictionary.cs
+++ Assets/1. Data Structure/02. Scripts/StudyDictionary.cs
@@ -1,42 +1,53 @@
-﻿using System.Collections.Generic;
-using System.IO.Pipes;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+public class PersonData
+{
+    public int age;
+    public string name;
+    public float height;
+    public float weight;
+
+    public PersonData(int age, string name, float height, float weight)
+    {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
 
 public class StudyDictionary : MonoBehaviour
 {
-    //커밋되돌리기
-
-    public Dictionary<string, int> persons = new Dictionary<string, int>();
+    public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
 
     private void Start()
     {
-        persons.Add("철수", 10);
-        persons.Add("영희", 15);
-        persons.Add("동수", 17);
+        persons.Add("철수", new PersonData(15, "철수", 170f, 60f));
+        persons.Add("영희", new PersonData(15, "영희", 170f, 60f));
+        persons.Add("동수", new PersonData(15, "동수", 170f, 60f));
 
-        int age = persons["철수"];
-        Debug.Log($"철수의 나이는 {age}입니다.");
+        Debug.Log(persons["철수"].age);
+        Debug.Log(persons["철수"].name);
+        Debug.Log(persons["철수"].height);
+        Debug.Log(persons["철수"].weight);
 
-        string name;
+        Hashtable h_nameAges = new Hashtable();
 
-        foreach( var person in persons )
+//추가
+        h_nameAges["철수"] = 10;
+        h_nameAges["영수"] = "20"; // 다른 타입 저장 가능 (비효율적)
+
+//데이터 출력 -> 캐스팅 필요
+        int score = (int)h_nameAges["철수"];
+
+//삭제
+        h_nameAges.Remove("Player2");
+
+        if (h_nameAges.ContainsKey("철수"))
         {
-            if(person.Value == 15)
-            {
-                Debug.Log($"나이가 15인 사람의 이름은 {person.Key}입니다.");
-            }
-
-            if(persons.ContainsKey("철수"))
-            {
-                Debug.Log("사람 중에 철수가 있음");
-                Debug.Log($"{person.Key}의 나이는 {person.Value}입니다.");
-            }
-
-            if(persons.ContainsValue(17))
-            {
-                Debug.Log("사람 중에 17살인 사람이 있음");
-                Debug.Log($"{person.Key}의 나이는 {person.Value}입니다.");
-            }
+            Debug.Log("HashTable에 철수가 있음.");
         }
     }
 }
