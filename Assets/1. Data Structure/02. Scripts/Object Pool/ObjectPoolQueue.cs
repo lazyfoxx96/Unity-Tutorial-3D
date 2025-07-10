7fda75937813f6edc9a72509fc59d51e8a6ee41c
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-07-10 11:45:14 +0900

    250710 - 1 - UIStack ObjectPooling

---
+++ Assets/1. Data Structure/02. Scripts/Object Pool/ObjectPoolQueue.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolQueue : MonoBehaviour
+{
+    public Queue<GameObject> objQueue = new Queue<GameObject>(); //오브젝트들이 들어갈 큐
+
+    public GameObject objPrefab; // 생성할 오브젝트
+    public Transform parent; // 계층 구조상 들어갈 부모 오브젝트
+
+    private void Start()
+    {
+        CreateObject();
+    }
+
+    private void CreateObject() // 오브젝트를 생성하는 기능 -> pool을 채우는 기능
+    {
+        for(int i = 0; i < 30; i++)
+        {
+            GameObject obj = Instantiate(objPrefab, parent);
+
+            EnqueueObject(obj);
+        }
+    }
+
+    public void EnqueueObject(GameObject newObj)
+    {
+        objQueue.Enqueue(newObj);
+        newObj.SetActive(false);
+    }
+
+    public GameObject DequeueObject()
+    {
+        if(objQueue.Count < 10) 
+            CreateObject();
+
+        GameObject obj = objQueue.Dequeue();
+        obj.SetActive(true);
+
+        return obj;
+    }
+}
