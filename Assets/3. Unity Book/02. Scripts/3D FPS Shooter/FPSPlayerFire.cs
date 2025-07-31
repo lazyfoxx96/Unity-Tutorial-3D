eb47773fe1d6342cff1398ee514544c57b485e05
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-07-31 09:20:15 +0900

    ~ 250730

--- Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
@@ -13,6 +13,16 @@ public class FPSPlayerFire : MonoBehaviour
     private ParticleSystem ps;
     private Animator anim;
 
+    public GameObject weapon01;
+    public GameObject weapon02;
+
+    public GameObject weapon01_R;
+    public GameObject weapon02_R;
+
+    public GameObject crosshair01;
+    public GameObject crosshair02;
+    public GameObject crosshair02_zoom;
+
     public TextMeshProUGUI wModeText;
     public GameObject[] eff_Flash;
 
@@ -35,9 +45,10 @@ public class FPSPlayerFire : MonoBehaviour
         if (FPSGameManager.Instance.gState != FPSGameManager.GameState.Run)
             return;
 
-        if (Input.GetMouseButtonDown(0))
+        #region 마우스 왼쪽 클릭 -> 총 발사
+        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
         {
-            if(anim.GetFloat("MoveMotion") == 0)
+            if (anim.GetFloat("MoveMotion") == 0)
                 anim.SetTrigger("Attack");
 
             StartCoroutine(ShootEffectOn(0.05f));
@@ -45,9 +56,9 @@ public class FPSPlayerFire : MonoBehaviour
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
             RaycastHit hitInfo = new RaycastHit();
 
-            if(Physics.Raycast(ray, out hitInfo))
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                if(hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy")) //Raycast를 Enemy가 맞은 경우
+                if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy")) //Raycast를 Enemy가 맞은 경우
                 {
                     EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
                     eFSM.HitEnemy(weaponPower);
@@ -61,8 +72,10 @@ public class FPSPlayerFire : MonoBehaviour
                 }
             }
         }
+        #endregion
 
-        if (Input.GetMouseButtonDown(1))
+        #region 마우스 오른쪽 클릭 -> 일반모드 - 수류탄 / 저격모드 - 조준경
+        if (Input.GetMouseButtonDown(1)) // 마우스 오른쪽 버튼 클릭
         {
             switch (wMode)
             {
@@ -74,35 +87,47 @@ public class FPSPlayerFire : MonoBehaviour
                     rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
                     break;
                 case WeaponMode.Sniper: // 저격 모드일 때 마우스 오른쪽 -> 확대/축소 조준경
-                    // if (!ZoomMode)
-                    // {
-                    //     Camera.main.fieldOfView = 15f;
-                    //     ZoomMode = true;
-                    // }
-                    // else
-                    // {
-                    //     Camera.main.fieldOfView = 60f;
-                    //     ZoomMode = false;
-                    // }
+                    zoomMode = !zoomMode; // 현재 줌 모드 상태 변경
 
-                    float fov = zoomMode ? 60f : 15f;
+                    float fov = zoomMode ? 15f : 60f;
                     Camera.main.fieldOfView = fov;
-                    zoomMode = !zoomMode;
+
+                    crosshair02_zoom.SetActive(zoomMode);
+                    crosshair02.SetActive(!zoomMode);
                     break;
             }
         }
+        #endregion
 
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        #region 무기 변경
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             wMode = WeaponMode.Normal;
             Camera.main.fieldOfView = 60f;
-            wModeText.text = "Nomal Mode";
+            wModeText.text = "Normal Mode";
+
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+            crosshair01.SetActive(true);
+            crosshair02.SetActive(false);
+            crosshair02_zoom.SetActive(false);
+            weapon01_R.SetActive(true);
+            weapon02_R.SetActive(false);
+
         }
         else if(Input.GetKeyDown(KeyCode.Alpha2))
         {
             wMode = WeaponMode.Sniper;
             wModeText.text = "Sniper Mode";
+
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+            weapon01_R.SetActive(false);
+            weapon02_R.SetActive(true);
+            crosshair01.SetActive(false);
+            crosshair02.SetActive(true);
         }
+        #endregion
     }
 
     IEnumerator ShootEffectOn(float duration)
